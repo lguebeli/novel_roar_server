af8e962fe68f5385e3aac58af11ab47e626ee70f
jluech <janik@luechinger.ch>
2022-10-31 17:15:50 +0100

    feat: add API for sending RW configs

---
+++ configurations.py
@@ -0,0 +1,20 @@
+import json
+import os
+import socket
+
+CLIENT_IP = "<Client-IP>"
+
+
+def send_config(config):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((CLIENT_IP, 42666))
+        sock.sendall(bytes(config, encoding="utf-8"))
+        print("sent", config)
+
+
+def map_to_ransomware_configuration(action):
+    nr_of_configs = os.listdir("./rw-configs")
+    assert 0 <= action <= nr_of_configs
+    with open(os.path.join(os.path.curdir, "./rw-configs/config-{act}.json".format(act=action)), "r") as conf_file:
+        config = json.loads(conf_file.read())
+    send_config(config)
