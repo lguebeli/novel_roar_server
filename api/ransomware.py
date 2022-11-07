acca022e1ac4c3509cf8555ad047d81afa78f3a7
jluech <janik@luechinger.ch>
2022-11-07 17:26:18 +0100

    feat: set RW_DONE to finish episode

--- api/ransomware.py
+++ api/ransomware.py
@@ -2,9 +2,13 @@ from http import HTTPStatus
 
 from flask import Blueprint
 
+from environment.state_handling import set_rw_done
+
+
 rw_bp = Blueprint("ransomware", __name__, url_prefix="/rw")
 
 
 @rw_bp.route("/done", methods=["PUT"])
 def mark_done():
+    set_rw_done()
     return "", HTTPStatus.NO_CONTENT
