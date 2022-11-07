fix: reward computation and anomaly detection

environment/anomaly_detection.py
@@ -12,23 +12,40 @@ from sklearn.preprocessing import StandardScaler
 # ========================================
 CSV_FILES_FOLDER = "C:/Users/jluec/Desktop/fingerprints"
 CONTAMINATION_FACTOR = 0.05
+ALL_CSV_HEADERS = "time,timestamp,seconds,connectivity,cpu_us,cpu_sy,cpu_ni,cpu_id,cpu_wa,cpu_hi,cpu_si,tasks_total,tasks_running,tasks_sleeping,tasks_stopped,tasks_zombie,mem_free,mem_used,mem_cache,swap_avail,net_lo_rx,net_lo_tx,net_eth_rx,net_eth_tx,cpu_temp,alarmtimer:alarmtimer_fired,alarmtimer:alarmtimer_start,block:block_bio_backmerge,block:block_bio_remap,block:block_dirty_buffer,block:block_getrq,block:block_touch_buffer,block:block_unplug,cachefiles:cachefiles_create,cachefiles:cachefiles_lookup,cachefiles:cachefiles_mark_active,clk:clk_set_rate,cpu-migrations,cs,dma_fence:dma_fence_init,fib:fib_table_lookup,filemap:mm_filemap_add_to_page_cache,gpio:gpio_value,ipi:ipi_raise,irq:irq_handler_entry,irq:softirq_entry,jbd2:jbd2_handle_start,jbd2:jbd2_start_commit,kmem:kfree,kmem:kmalloc,kmem:kmem_cache_alloc,kmem:kmem_cache_free,kmem:mm_page_alloc,kmem:mm_page_alloc_zone_locked,kmem:mm_page_free,kmem:mm_page_pcpu_drain,mmc:mmc_request_start,net:net_dev_queue,net:net_dev_xmit,net:netif_rx,page-faults,pagemap:mm_lru_insertion,preemptirq:irq_enable,qdisc:qdisc_dequeue,qdisc:qdisc_dequeue,random:get_random_bytes,random:mix_pool_bytes_nolock,random:urandom_read,raw_syscalls:sys_enter,raw_syscalls:sys_exit,rpm:rpm_resume,rpm:rpm_suspend,sched:sched_process_exec,sched:sched_process_free,sched:sched_process_wait,sched:sched_switch,sched:sched_wakeup,signal:signal_deliver,signal:signal_generate,skb:consume_skb,skb:consume_skb,skb:kfree_skb,skb:kfree_skb,skb:skb_copy_datagram_iovec,sock:inet_sock_set_state,task:task_newtask,tcp:tcp_destroy_sock,tcp:tcp_probe,timer:hrtimer_start,timer:timer_start,udp:udp_fail_queue_rcv_skb,workqueue:workqueue_activate_work,writeback:global_dirty_state,writeback:sb_clear_inode_writeback,writeback:wbc_writepage,writeback:writeback_dirty_inode,writeback:writeback_dirty_inode_enqueue,writeback:writeback_dirty_page,writeback:writeback_mark_inode_dirty,writeback:writeback_pages_written,writeback:writeback_single_inode,writeback:writeback_write_inode,writeback:writeback_written"
+DUPLICATE_HEADERS = ["qdisc:qdisc_dequeue", "skb:consume_skb", "skb:kfree_skb"]
+
+# TODO: remove for RPi
+cut_events_for_vm = ["cachefiles:cachefiles_create", "cachefiles:cachefiles_lookup", "cachefiles:cachefiles_mark_active", "ipi:ipi_raise", "preemptirq:irq_enable", "random:get_random_bytes", "random:mix_pool_bytes_nolock", "random:urandom_read"]
 
 # ========================================
 # ==========   GLOBALS   ==========
 # ========================================
 CLASSIFIER = None
+SCALER = None
 
 
-def get_classifier():
+def __get_classifier():
     global CLASSIFIER
     if not CLASSIFIER:
         CLASSIFIER = IForest(random_state=42, contamination=CONTAMINATION_FACTOR)
     return CLASSIFIER
 
 
+def __init_scaler(train_set):
+    global SCALER
+    SCALER = StandardScaler().fit(train_set)  # Feature scaling
+
+
+def __get_scaler():
+    global SCALER
+    assert SCALER is not None, "Must first initialize scaler and fit to training set!"
+    return SCALER
+
+
 def preprocess_dataset(dataset):
     # Remove duplicates: 'qdisc:qdisc_dequeue', 'skb:consume_skb', 'skb:kfree_skb'
-    dataset.drop(["qdisc:qdisc_dequeue.1", "skb:consume_skb.1", "skb:kfree_skb.1"], inplace=True, axis=1)
+    dataset.drop(list(map(lambda header: header+".1", DUPLICATE_HEADERS)), inplace=True, axis=1)  # read_csv adds the .1
     # Remove temporal features
     dataset.drop(["time", "timestamp", "seconds"], inplace=True, axis=1)
 
@@ -66,7 +83,7 @@ def scale_dataset(scaler, dataset):
 
 
 def evaluate_dataset(name, dataset):
-    clf = get_classifier()
+    clf = __get_classifier()
     pred = clf.predict(dataset)
     unique_elements, counts_elements = np.unique(pred, return_counts=True)
     print(name, "\t", unique_elements, "\t", counts_elements)
@@ -90,14 +107,15 @@ def train_anomaly_detection():
 
     # Scale the datasets, turning them into ndarrays
     print("Scaling dataset features to fit training set.")
-    scaler = StandardScaler().fit(train_set)  # Feature scaling
+    __init_scaler(train_set)
+    scaler = __get_scaler()
     train_set = scale_dataset(scaler, train_set)
     test_set = scale_dataset(scaler, test_set)
     infected_c1_data = scale_dataset(scaler, infected_c1_data)
 
     # Instantiate ML Isolation Forest instance
     print("Instantiate classifier.")
-    clf = get_classifier()
+    clf = __get_classifier()
 
     # Train model
     print("Train classifier on training set.")
@@ -110,11 +128,37 @@ def train_anomaly_detection():
     evaluate_dataset("inf-c1", infected_c1_data)
 
 
-def detect_anomaly(fingerprint):
-    clf = get_classifier()
+# TODO: remove for RPi
+def adjust_data_from_vm(fp_data):
+    for event in cut_events_for_vm:
+        idx = ALL_CSV_HEADERS.split(",").index(event)
+        fp_data = np.insert(fp_data, idx, 0.0)
+    return fp_data
+
+
+def detect_anomaly(fingerprint):  # string
+    print("Detecting anomaly.")
+    clf = __get_classifier()
+
+    # Transforming FP string to pandas DataFrame
+    fp_data = adjust_data_from_vm(fingerprint)  # TODO: remove for RPi
+
+    fp_data = fp_data.reshape(1, -1)
+
+    headers = ALL_CSV_HEADERS.split(",")
+    for header in DUPLICATE_HEADERS:
+        found = headers.index(header)
+        headers[found+1] = headers[found+1]+".1"  # match the .1 for duplicates appended by read_csv()
+
+    df_fp = pd.DataFrame(fp_data, columns=headers)
+
+    # Sanitizing FP to match IsolationForest
+    preprocessed = preprocess_dataset(df_fp)
+    scaler = __get_scaler()
+    scaled = scale_dataset(scaler, preprocessed)
+    print("Scaled FP to", scaled.shape)
 
     # Evaluate fingerprint
-    single_sample = fingerprint.reshape(1, -1)
-    pred = clf.predict(single_sample)
+    pred = clf.predict(scaled)
     assert type(pred) == np.ndarray and len(pred) == 1
     return pred[0]

environment/controller.py
@@ -8,7 +8,8 @@ from environment.state_handling import is_fp_ready, set_fp_ready, is_rw_done, co
 
 
 def transform_fp(fp):
-    return np.asarray(fp)
+    split_to_floats = list(map(lambda feat: float(feat), fp.split(",")))
+    return np.asarray(split_to_floats)
 
 
 def loop_episode(agent):
@@ -28,10 +29,12 @@ def loop_episode(agent):
         state = transform_fp(curr_fp)
 
         # agent selects action based on state
+        print("Predict next action.")
         selected_action = agent.predict(state)
 
         # convert action to config and send to client
         if selected_action != last_action:
+            print("Sending new action {} to client.".format(selected_action))
             config = map_to_ransomware_configuration(selected_action)
             send_config(config)
         last_action = selected_action
@@ -46,8 +49,8 @@ def loop_episode(agent):
         set_fp_ready(False)
 
         # compute reward based on FP
-        # reward = compute_reward(next_fp, RW_DONE, selected_action)  # TODO: including action required?
-        reward = compute_reward(next_fp, is_rw_done(), selected_action)  # TODO: including action required?
+        print("Computing reward for FP.")
+        reward = compute_reward(transform_fp(next_fp), is_rw_done(), selected_action)  # TODO: including action required?
 
         # send reward to agent, update weights accordingly
         agent.update_weights(reward)