fix: correct error computation and fiddle with agent params

agent/abstract_agent.py
@@ -1,12 +1,26 @@
+import os
 from abc import ABC, abstractmethod
 
 import numpy as np
+
+CSV_FOLDER_PATH = "<local-path-to-collected-fingerprints-folders>"
+RPI_MODEL_PREFIX = "mod4-"
 
 
 class AbstractAgent(ABC):
     @staticmethod
     def standardize_inputs(inputs):
-        return np.log(inputs + 0.00001)  # lower value span
+        csv_normal = np.loadtxt(fname=os.path.join(CSV_FOLDER_PATH, RPI_MODEL_PREFIX + "normal-behavior.csv"), delimiter=",", skiprows=1)
+        norm_min = np.min(csv_normal, axis=0).reshape(-1, 1)
+        norm_max = np.max(csv_normal, axis=0).reshape(-1, 1)
+        # print("ABSAGENT: normal min/max", csv_normal.shape, norm_min.shape, norm_max.shape)
+        # print("ABSAGENT: min/max min/max", norm_min, np.min(norm_min), np.max(norm_min), norm_max, np.min(norm_max), np.max(norm_max))
+
+        std = (inputs - norm_min) / (norm_max - norm_min + 0.001)
+        # print("ABSAGENT: input min/max", np.min(inputs), np.max(inputs))
+        # print("ABSAGENT: std min/max", np.min(std), np.max(std))
+
+        return std
 
     @abstractmethod
     def predict(self, fingerprint):

v2/agent/agent.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 
 from agent.abstract_agent import AbstractAgent
@@ -5,46 +7,56 @@ from environment.state_handling import get_num_configs
 from v2.agent.model import Model
 
 EPSILON = 0.2
-LEARN_RATE = 0.01  # 0.0035
+LEARN_RATE = 0.05  # 0.0035
 DECAY_RATE = 0.01  # 0.0005
-DISCOUNT_FACTOR = 0.85
+DISCOUNT_FACTOR = 0.5  # 0.85
+
+CSV_FOLDER_PATH = "<local-path-to-collected-fingerprints-folders>"
+RPI_MODEL_PREFIX = "mod4-"
 
 
 class Agent2QLearning(AbstractAgent):
     def __init__(self):
-        self.model = Model(epsilon=0, learn_rate=0, decay_rate=0)
+        self.model = Model(epsilon=EPSILON, learn_rate=LEARN_RATE, decay_rate=DECAY_RATE)
+        self.steps = 0
 
         num_configs = get_num_configs()
         self.actions = list(range(num_configs))
-        self.selected_action = 0
-        # self.best_action = 0  # TODO: v3 - check if required, maybe only local in predict stored in one var
-
         self.output_size = num_configs
-        self.q_values = np.zeros(num_configs)
 
-        self.steps = 0
+    def __crop_fp(self, fp):  # TODO: v3 - remove simplification
+        with open(os.path.join(CSV_FOLDER_PATH, RPI_MODEL_PREFIX + "normal-behavior.csv"), "r") as csv_normal:
+            csv_headers = csv_normal.read().split(",")
+        # headers = ["cpu_id", "tasks_running", "mem_free", "cpu_temp", "block:block_bio_remap",
+        #            "sched:sched_process_exec", "writeback:writeback_pages_written"]
+        headers = ["cpu_id", "tasks_running", "mem_free", "cpu_temp"]
+        indexes = []
+        for header in headers:
+            indexes.append(csv_headers.index(header))
+        return fp[indexes]
 
     def predict(self, fingerprint):
         std_fp = AbstractAgent.standardize_inputs(fingerprint)
+        cropped_fp = self.__crop_fp(std_fp)  # TODO: v3 - remove simplification
         self.steps += 1
-        self.selected_action, self.q_values = self.model.forward(std_fp, self.steps)
+        selected_action, q_values = self.model.forward(cropped_fp, self.steps)
+        best_action = np.argmax(q_values)
 
-        # TODO: v3 - save predicted and optimal action
-        return self.selected_action
+        return selected_action, best_action, q_values
 
     def update_weights(self, fingerprint, error):
         std_fp = AbstractAgent.standardize_inputs(fingerprint)
-        self.model.backward(std_fp, error)
+        cropped_fp = self.__crop_fp(std_fp)  # TODO: v3 - remove simplification
+        self.model.backward(cropped_fp, error)
 
     def init_error(self):
         return np.zeros((self.output_size, 1))
 
-    def update_error(self, error, next_action, reward, is_done):
-        # print("AGENT q err:", self.q_values.shape, error.shape)
+    def update_error(self, error, reward, is_done, selected_action, selected_q_value, best_next_action, best_next_q_value):
+        print("AGENT: R sel selval best bestval", reward, selected_action, selected_q_value, best_next_action, best_next_q_value)
         if is_done:
-            error[self.selected_action] = reward - self.q_values[self.selected_action]
+            error[selected_action] = reward - selected_q_value
         else:
-            error[self.selected_action] = \
-                reward + (DISCOUNT_FACTOR * np.max(next_action)) - self.q_values[self.selected_action]
-        # print("AGENT err:", error)
+            error[selected_action] = reward + (DISCOUNT_FACTOR * best_next_q_value) - selected_q_value
+        print("AGENT: err\n", error.T)
         return error

v2/agent/model.py
@@ -2,11 +2,11 @@ import numpy as np
 
 from environment.state_handling import get_num_configs
 
-FP_DIMS = 103
-HIDDEN_NEURONS = 200
+# FP_DIMS = 103
+# HIDDEN_NEURONS = 200
+FP_DIMS = 4  # TODO: v3 - remove simplification
+HIDDEN_NEURONS = 10  # TODO: v3 - remove simplification
 NUM_CONFIGS = get_num_configs()
-
-ELU_ALPHA = 1
 
 
 class Model(object):
@@ -38,33 +38,44 @@ class Model(object):
         self.q = np.ones((num_output,))
 
     def forward(self, inputs, step_n):
-        # print("MODEL: inputs", inputs.shape, np.min(inputs), np.argmin(inputs), np.max(inputs), np.argmax(inputs))
-        # print("MODEL: w1", self.weights1.shape)
+        # print("MODEL: inputs", inputs.shape, np.min(inputs), np.argmin(inputs), np.max(inputs), np.argmax(inputs), inputs)
+
+        # ==============================
+        # Q-VALUES
+        # ==============================
 
         # Forward pass through neural network to compute Q-values
+        # print("MODEL: w1", self.weights1.shape, self.weights1)
         h1 = np.dot(self.weights1.T, inputs) + self.bias_weights1
+        # print("MODEL: h1 dot", np.dot(self.weights1.T, inputs))
         # print("MODEL: h1 min/max", h1.shape, np.min(h1), np.argmin(h1), np.max(h1), np.argmax(h1))
-        self.x1 = h1 * (h1 > 0)  # ReLU activation, x if x > 0 else 0
+        # self.x1 = h1 * (h1 > 0)  # ReLU activation, x if x > 0 else 0
+        self.x1 = 1 / (1 + np.exp(-h1))  # logistic activation
 
-        # print("MODEL: x1 min/max", self.x1.shape, np.min(self.x1), np.argmin(self.x1), np.max(self.x1),
-        #       np.argmax(self.x1))
-        # print("MODEL: w2", self.weights2.shape)
+        # print("MODEL: x1 min/max", self.x1.shape, np.min(self.x1), np.argmin(self.x1), np.max(self.x1), np.argmax(self.x1))
+        # print("MODEL: w2", self.weights2.shape, self.weights2)
 
         h2 = np.dot(self.weights2.T, self.x1) + self.bias_weights2
+        # print("MODEL: h2 dot", np.dot(self.weights2.T, self.x1))
         # print("MODEL: h2 min/max", h2.shape, np.min(h2), np.argmin(h2), np.max(h2), np.argmax(h2))
-        self.q = h2 * (h2 > 0)  # ReLU activation, x2
+        # self.q = h2 * (h2 > 0)  # ReLU activation, x2
+        self.q = 1 / (1 + np.exp(-h2))  # logistic activation, x2
 
-        # print("MODEL: Q", self.q.shape, self.q)
+        print("MODEL: Q", self.q.shape, "\n", self.q)
+
+        # ==============================
+        # POLICY
+        # ==============================
 
         # Choose action based on epsilon-greedy policy
-        possible_a = self.allowed_actions
+        possible_a = self.allowed_actions  # technically an array of indexes
         q_a = self.q[possible_a]
 
         if np.random.random() < self.epsilon_forward:  # explore randomly
-            # print("MODEL: random action")
+            print("MODEL: random action")
             sel_a = possible_a[np.random.randint(possible_a.size)]
         else:  # exploit greedily
-            # print("MODEL: greedy action")
+            print("MODEL: greedy action")
             argmax = np.argmax(q_a)
             # print("MODEL: argmax", argmax, "of", q_a, "for", possible_a)
             sel_a = possible_a[argmax]
@@ -77,18 +88,35 @@ class Model(object):
 
     def backward(self, fingerprint, q_err):
         # Backpropagation of error through neural network
-        # print("MODEL back: fp err", fingerprint.shape, q_err.shape)
-        delta2 = (self.q > 0) * q_err  # derivative ReLU: 1 if x > 0 else 0
-        # print("MODEL back: d2", delta2.shape)
-        delta_weights2 = np.outer(self.x1, delta2.T)
-        # print("MODEL back: dw2", delta_weights2.shape)
 
-        delta1 = (self.x1 > 0) * np.dot(self.weights2, delta2)  # ReLU
-        # print("MODEL back: d1", delta1.shape)
+        # ==============================
+        # COMPUTE DELTA
+        # ==============================
+
+        # delta2 = (self.q > 0) * q_err  # derivative ReLU: 1 if x > 0 else 0
+        delta2 = self.q * (1 - self.q) * q_err  # derivative logistic: f(x) * (1 - f(x))
+        delta_weights2 = np.outer(self.x1, delta2.T)
+        # print("MODEL back: fp err", fingerprint.shape, q_err.shape, fingerprint, q_err)
+        # print("MODEL back: d2", delta2.shape, delta2)
+        # print("MODEL back: dw2", delta_weights2.shape, delta_weights2)
+
+        # delta1 = (self.x1 > 0) * np.dot(self.weights2, delta2)  # ReLU
+        delta1 = self.x1 * (1 - self.x1) * np.dot(self.weights2, delta2)  # logistic
         delta_weights1 = np.outer(fingerprint, delta1)
-        # print("MODEL back: dw1", delta_weights1.shape)
+        # print("MODEL back: d1", delta1.shape, delta1)
+        # print("MODEL back: dw1", delta_weights1.shape, delta_weights1)
+
+        # ==============================
+        # UPDATE WEIGHTS
+        # ==============================
+
+        # print("MODEL: weights1 before", self.weights1.shape, np.min(self.weights1), np.argmin(self.weights1), np.max(self.weights1), np.argmax(self.weights1))
+        # print("MODEL: weights2 before", self.weights2.shape, np.min(self.weights2), np.argmin(self.weights2), np.max(self.weights2), np.argmax(self.weights2))
 
         self.weights1 += self.learn_rate * delta_weights1
         self.weights2 += self.learn_rate * delta_weights2
         self.bias_weights1 += self.learn_rate * delta1
         self.bias_weights2 += self.learn_rate * delta2
+
+        # print("MODEL: weights1 after", self.weights1.shape, np.min(self.weights1), np.argmin(self.weights1), np.max(self.weights1), np.argmax(self.weights1))
+        # print("MODEL: weights2 after", self.weights2.shape, np.min(self.weights2), np.argmin(self.weights2), np.max(self.weights2), np.argmax(self.weights2))