fab3dbd1156d711c229bd00e8b79357e748cb5cb
jluech <janik@luechinger.ch>
2022-11-28 13:39:10 +0100

    refactor: cleanup unused instance folder

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,3 @@
-import os
-
 from flask import Flask
 
 from . import status, fingerprint, ransomware
@@ -8,12 +6,6 @@ from . import status, fingerprint, ransomware
 def create_app():
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
     app.register_blueprint(status.status_bp)
     app.register_blueprint(fingerprint.fp_bp)
