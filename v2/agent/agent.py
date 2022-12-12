fix: use proper Q-values for backprop

v2/agent/agent.py
@@ -8,17 +8,24 @@ from environment.state_handling import get_num_configs
 from v2.agent.model import ModelQLearning
 
 EPSILON = 0.2
-LEARN_RATE = 0.025  # simpl: 0.05; full: 0.025
+LEARN_RATE = 0.05  # simpl: 0.05; full: 0.025
 DISCOUNT_FACTOR = 0.75  # simpl: 0.5; full: 0.75
+
+FP_DIMS = 103  # simpl: 7; full: 103
+HIDDEN_NEURONS = 60  # simpl: 10; full: 60
 
 
 class AgentQLearning(AbstractAgent):
     def __init__(self):
-        self.model = ModelQLearning(epsilon=EPSILON, learn_rate=LEARN_RATE)
-
         num_configs = get_num_configs()
         self.actions = list(range(num_configs))
         self.output_size = num_configs
+
+        self.num_input = FP_DIMS  # Input size
+        self.num_hidden = HIDDEN_NEURONS  # Hidden neurons
+        self.num_output = num_configs  # Output size
+
+        self.model = ModelQLearning(epsilon=EPSILON, learn_rate=LEARN_RATE, num_configs=num_configs)
 
     def __crop_fp(self, fp):  # TODO: v3 - remove simplification
         with open(os.path.join(CSV_FOLDER_PATH, RPI_MODEL_PREFIX + "normal-behavior.csv"), "r") as csv_normal:
@@ -30,29 +37,35 @@ class AgentQLearning(AbstractAgent):
             indexes.append(csv_headers.index(header))
         return fp[indexes]
 
-    def predict(self, fingerprint):
-        std_fp = AbstractAgent.standardize_inputs(fingerprint)
-        cropped_fp = self.__crop_fp(std_fp)  # TODO: v3 - remove simplification
-        selected_action, q_values = self.model.forward(std_fp)
-        best_action = np.argmax(q_values)
+    def initialize_network(self):
+        weights1 = np.random.uniform(0, 1, (self.num_input, self.num_hidden))
+        weights2 = np.random.uniform(0, 1, (self.num_hidden, self.num_output))
 
-        return selected_action, best_action, q_values
+        bias_weights1 = np.zeros((self.num_hidden, 1))
+        bias_weights2 = np.zeros((self.num_output, 1))
 
-    def update_weights(self, fingerprint, error):
-        std_fp = AbstractAgent.standardize_inputs(fingerprint)
+        return weights1, weights2, bias_weights1, bias_weights2
+
+    def predict(self, weights1, weights2, bias_weights1, bias_weights2, inputs):
+        std_fp = AbstractAgent.standardize_inputs(inputs)
         cropped_fp = self.__crop_fp(std_fp)  # TODO: v3 - remove simplification
-        self.model.backward(std_fp, error)
+        hidden, q_values, selected_action = self.model.forward(weights1, weights2, bias_weights1, bias_weights2, inputs=std_fp)
+        return hidden, q_values, selected_action
+
+    def update_weights(self, q_values, error, state, hidden, weights1, weights2, bias_weights1, bias_weights2):
+        std_fp = AbstractAgent.standardize_inputs(state)
+        cropped_fp = self.__crop_fp(std_fp)  # TODO: v3 - remove simplification
+        self.model.backward(q_values, error, hidden, weights1, weights2, bias_weights1, bias_weights2, inputs=std_fp)
 
     def init_error(self):
         return np.zeros((self.output_size, 1))
 
-    def update_error(self, error, reward, is_done, selected_action, selected_q_value, best_next_action,
-                     best_next_q_value):
-        # print("AGENT: R sel selval best bestval", reward, selected_action, selected_q_value, best_next_action,
-        #       best_next_q_value)
+    def update_error(self, error, reward, selected_action, curr_q_values, next_q_values, is_done):
+        # print("AGENT: R sel selval best bestval", reward, selected_action, curr_q_values, next_q_values)
         if is_done:
-            error[selected_action] = reward - selected_q_value
+            error[selected_action] = reward - curr_q_values[selected_action]
         else:
-            error[selected_action] = reward + (DISCOUNT_FACTOR * best_next_q_value) - selected_q_value  # off-policy
-        # print("AGENT: err\n", error.T)
+            # off-policy
+            error[selected_action] = reward + (DISCOUNT_FACTOR * np.max(next_q_values)) - curr_q_values[selected_action]
+        print("AGENT: err\n", error.T)
         return error

v2/agent/model.py
@@ -8,7 +8,7 @@ NUM_CONFIGS = get_num_configs()
 
 
 class ModelQLearning(object):
-    def __init__(self, epsilon, learn_rate):
+    def __init__(self, epsilon, learn_rate, num_configs):
         # Initialize random seed for reproducibility
         np.random.seed(42)
 
@@ -16,24 +16,12 @@ class ModelQLearning(object):
         self.epsilon = epsilon
         self.learn_rate = learn_rate
 
-        # Initialize neural network
-        self.allowed_actions = np.asarray(range(NUM_CONFIGS))
+        # Initialize action set
+        self.allowed_actions = np.asarray(range(num_configs))
 
-        num_input = FP_DIMS  # Input size
-        num_hidden = HIDDEN_NEURONS  # Hidden neurons
-        num_output = NUM_CONFIGS  # Output size
-
-        self.weights1 = np.random.uniform(0, 1, (num_input, num_hidden))
-        self.weights2 = np.random.uniform(0, 1, (num_hidden, num_output))
-
-        self.bias_weights1 = np.zeros((num_hidden, 1))
-        self.bias_weights2 = np.zeros((num_output, 1))
-
-        self.x1 = np.ones((num_hidden,))
-        self.q = np.ones((num_output,))
-
-    def forward(self, inputs):
-        # print("MODEL: inputs", inputs.shape, np.min(inputs), np.argmin(inputs), np.max(inputs), np.argmax(inputs), inputs)
+    def forward(self, weights1, weights2, bias_weights1, bias_weights2, inputs):
+        # print("MODEL: inputs", inputs.shape, inputs)
+        print("MODEL: inputs min/max", inputs.shape, np.min(inputs), np.argmin(inputs), np.max(inputs), np.argmax(inputs))
 
         # ==============================
         # Q-VALUES
@@ -41,25 +29,26 @@ class ModelQLearning(object):
         # simpl: Log/Log; full: ReLU/Log
 
         # Forward pass through neural network to compute Q-values
-        # print("MODEL: w1", self.weights1.shape, self.weights1)
-        # print("MODEL: w1 min/max", self.weights1.shape, np.min(self.weights1), np.argmin(self.weights1), np.max(self.weights1), np.argmax(self.weights1))
-        h1 = np.dot(self.weights1.T, inputs) + self.bias_weights1
-        # print("MODEL: h1 dot", np.dot(self.weights1.T, inputs))
-        # print("MODEL: h1 min/max", h1.shape, np.min(h1), np.argmin(h1), np.max(h1), np.argmax(h1))
-        self.x1 = h1 * (h1 > 0)  # ReLU activation, x if x > 0 else 0
-        # self.x1 = 1 / (1 + np.exp(-h1))  # logistic activation
+        # print("MODEL: w1", weights1.shape, weights1)
+        print("MODEL: w1 min/max", weights1.shape, np.min(weights1), np.argmin(weights1), np.max(weights1), np.argmax(weights1))
+        print("MODEL: bw1 min/max", bias_weights1.shape, np.min(bias_weights1), np.argmin(bias_weights1), np.max(bias_weights1), np.argmax(bias_weights1))
+        adaline1 = np.dot(weights1.T, inputs) + bias_weights1
+        # print("MODEL: ad1 dot", np.dot(weights1.T, inputs))
+        print("MODEL: ad1 min/max", adaline1.shape, np.min(adaline1), np.argmin(adaline1), np.max(adaline1), np.argmax(adaline1))
+        # hidden = adaline1 * (adaline1 > 0)  # ReLU activation, x if a > 0 else 0
+        hidden = 1 / (1 + np.exp(-adaline1))  # logistic activation
+        print("MODEL: hidden min/max", hidden.shape, np.min(hidden), np.argmin(hidden), np.max(hidden), np.argmax(hidden))
 
-        # print("MODEL: x1 min/max", self.x1.shape, np.min(self.x1), np.argmin(self.x1), np.max(self.x1), np.argmax(self.x1))
-        # print("MODEL: w2", self.weights2.shape, self.weights2)
-        # print("MODEL: w2 min/max", self.weights2.shape, np.min(self.weights2), np.argmin(self.weights2), np.max(self.weights2), np.argmax(self.weights2))
+        # print("MODEL: w2", weights2.shape, weights2)
+        print("MODEL: w2 min/max", weights2.shape, np.min(weights2), np.argmin(weights2), np.max(weights2), np.argmax(weights2))
+        print("MODEL: bw2 min/max", bias_weights2.shape, np.min(bias_weights2), np.argmin(bias_weights2), np.max(bias_weights2), np.argmax(bias_weights2))
+        adaline2 = np.dot(weights2.T, hidden) + bias_weights2
+        # print("MODEL: ad2 dot", np.dot(weights2.T, hidden))
+        print("MODEL: ad2 min/max", adaline2.shape, np.min(adaline2), np.argmin(adaline2), np.max(adaline2), np.argmax(adaline2))
+        # q = adaline2 * (adaline2 > 0)  # ReLU activation, h2
+        q = 1 / (1 + np.exp(-adaline2))  # logistic activation, h2
 
-        h2 = np.dot(self.weights2.T, self.x1) + self.bias_weights2
-        # print("MODEL: h2 dot", np.dot(self.weights2.T, self.x1))
-        # print("MODEL: h2 min/max", h2.shape, np.min(h2), np.argmin(h2), np.max(h2), np.argmax(h2))
-        # self.q = h2 * (h2 > 0)  # ReLU activation, x2
-        self.q = 1 / (1 + np.exp(-h2))  # logistic activation, x2
-
-        # print("MODEL: Q", self.q.shape, "\n", self.q)
+        print("MODEL: Q", q.shape, "\n", q)
 
         # ==============================
         # POLICY
@@ -67,7 +56,7 @@ class ModelQLearning(object):
 
         # Choose action based on epsilon-greedy policy
         possible_a = self.allowed_actions  # technically an array of indexes
-        q_a = self.q[possible_a]
+        q_a = q[possible_a]
 
         if np.random.random() < self.epsilon:  # explore randomly
             print("MODEL: random action")
@@ -78,9 +67,9 @@ class ModelQLearning(object):
             # print("MODEL: argmax", argmax, "of", q_a, "for", possible_a)
             sel_a = possible_a[argmax]
 
-        return sel_a, self.q
+        return hidden, q, sel_a
 
-    def backward(self, fingerprint, q_err):
+    def backward(self, q, q_err, hidden, weights1, weights2, bias_weights1, bias_weights2, inputs):
         # Backpropagation of error through neural network
 
         # ==============================
@@ -88,30 +77,34 @@ class ModelQLearning(object):
         # ==============================
         # simpl: Log/Log; full: Log/ReLU
 
-        # print("MODEL back: fp err", fingerprint.shape, q_err.shape, fingerprint, q_err)
-        # delta2 = (self.q > 0) * q_err  # derivative ReLU: 1 if x > 0 else 0
-        delta2 = self.q * (1 - self.q) * q_err  # derivative logistic: f(x) * (1 - f(x))
-        delta_weights2 = np.outer(self.x1, delta2.T)
-        # print("MODEL back: d2", delta2.shape, delta2)
+        print("MODEL back: inputs err", inputs.shape, q_err.shape, inputs.T, q_err.T, sep="\n")
+        # delta2 = (q > 0) * q_err  # derivative ReLU: 1 if q > 0 else 0
+        delta2 = q * (1 - q) * q_err  # derivative logistic: f(x) * (1 - f(x))
+        delta_weights2 = np.outer(hidden, delta2.T)
+        print("MODEL back: d2", delta2.shape, delta2)
+        print("MODEL back: d2 min/max", delta2.shape, np.min(delta2), np.argmin(delta2), np.max(delta2), np.argmax(delta2))
         # print("MODEL back: dw2", delta_weights2.shape, delta_weights2)
+        print("MODEL back: dw2 min/max", delta_weights2.shape, np.min(delta_weights2), np.argmin(delta_weights2), np.max(delta_weights2), np.argmax(delta_weights2))
 
-        delta1 = (self.x1 > 0) * np.dot(self.weights2, delta2)  # ReLU
-        # delta1 = self.x1 * (1 - self.x1) * np.dot(self.weights2, delta2)  # logistic
-        delta_weights1 = np.outer(fingerprint, delta1)
+        # delta1 = (hidden > 0) * np.dot(weights2, delta2)  # ReLU
+        delta1 = hidden * (1 - hidden) * np.dot(weights2, delta2)  # logistic
+        delta_weights1 = np.outer(inputs, delta1)
         # print("MODEL back: d1", delta1.shape, delta1)
+        print("MODEL back: d1 min/max", delta1.shape, np.min(delta1), np.argmin(delta1), np.max(delta1), np.argmax(delta1))
         # print("MODEL back: dw1", delta_weights1.shape, delta_weights1)
+        print("MODEL back: dw1 min/max", delta_weights1.shape, np.min(delta_weights1), np.argmin(delta_weights1), np.max(delta_weights1), np.argmax(delta_weights1))
 
         # ==============================
         # UPDATE WEIGHTS
         # ==============================
 
-        # print("MODEL: weights1 before", self.weights1.shape, np.min(self.weights1), np.argmin(self.weights1), np.max(self.weights1), np.argmax(self.weights1))
-        # print("MODEL: weights2 before", self.weights2.shape, np.min(self.weights2), np.argmin(self.weights2), np.max(self.weights2), np.argmax(self.weights2))
+        # print("MODEL: weights1 before", weights1.shape, np.min(weights1), np.argmin(weights1), np.max(weights1), np.argmax(weights1))
+        # print("MODEL: weights2 before", weights2.shape, np.min(weights2), np.argmin(weights2), np.max(weights2), np.argmax(weights2))
 
-        self.weights1 += self.learn_rate * delta_weights1
-        self.weights2 += self.learn_rate * delta_weights2
-        self.bias_weights1 += self.learn_rate * delta1
-        self.bias_weights2 += self.learn_rate * delta2
+        weights1 += self.learn_rate * delta_weights1
+        weights2 += self.learn_rate * delta_weights2
+        bias_weights1 += self.learn_rate * delta1
+        bias_weights2 += self.learn_rate * delta2
 
-        # print("MODEL: weights1 after", self.weights1.shape, np.min(self.weights1), np.argmin(self.weights1), np.max(self.weights1), np.argmax(self.weights1))
-        # print("MODEL: weights2 after", self.weights2.shape, np.min(self.weights2), np.argmin(self.weights2), np.max(self.weights2), np.argmax(self.weights2))
+        # print("MODEL: weights1 after", weights1.shape, np.min(weights1), np.argmin(weights1), np.max(weights1), np.argmax(weights1))
+        # print("MODEL: weights2 after", weights2.shape, np.min(weights2), np.argmin(weights2), np.max(weights2), np.argmax(weights2))