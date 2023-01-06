308ddf00d5a558208473a6a4e921576d3d9802b4
jluech <janik@luechinger.ch>
2023-01-06 17:33:27 +0100

    refactor: select simulated enc rate from config instead of metrics

--- utilities/simulate.py
+++ utilities/simulate.py
@@ -1,3 +1,4 @@
+import json
 import os
 import random
 
@@ -10,22 +11,13 @@ from utilities.metrics import write_metrics_to_file
 # SIMULATE CLIENT BEHAVIOR
 # ==============================
 
-def simulate_sending_fp(config):
-    metrics_dir = os.path.join(TRAINING_CSV_FOLDER_PATH, "metrics")
-    with open(os.path.join(metrics_dir, "metrics-c{}.txt".format(config))) as metrics_file:
-        headers = metrics_file.readline().split(",")
-        rate_idx = headers.index("burst_current_rate")
-        # print("SIM: rate idx", config, rate_idx)
-        metrics_lines = metrics_file.read().split("\n")
-        metrics_lines.remove("")
-        metrics = list(map(lambda metric_line: metric_line.split(","), metrics_lines))
-        # print("SIM: metrics", metrics)
-        metric = random.choice(metrics)
-        # print("SIM: metric", metric)
-        rate = float(metric[rate_idx])
-        # print("SIM: rate", rate)
+def simulate_sending_fp(config_num):
+    config_dir = os.path.join(os.curdir, "rw-configs")
+    with open(os.path.join(config_dir, "config-{}.json".format(config_num)), "r") as config_file:
+        config = json.load(config_file)
+        rate = int(config["rate"])
 
-    config_fp_dir = os.path.join(TRAINING_CSV_FOLDER_PATH, "infected-c{}".format(config))
+    config_fp_dir = os.path.join(TRAINING_CSV_FOLDER_PATH, "infected-c{}".format(config_num))
     fp_files = os.listdir(config_fp_dir)
     with open(os.path.join(config_fp_dir, random.choice(fp_files))) as fp_file:
         # print("SIM: fp", fp_file.name)
